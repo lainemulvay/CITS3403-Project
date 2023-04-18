758becdb1289e44ba330be60df1c0f28c6f448e1
Vicky Chow <vicky.chow.senmedia@gmail.com>
2023-04-19 02:26:29 +0800

    Login and Reg page update

--- Hank/Login/scripts.js
+++ Hank/Login/scripts.js
@@ -1,19 +1,19 @@
-function loginButton() {
-    var loginDiv = document.getElementById("login");
-    var descriptionsDiv = document.getElementById("descriptions");
-    var introButton = document.getElementById("intro_button");
+// function loginButton() {
+//     var loginDiv = document.getElementById("login");
+//     var descriptionsDiv = document.getElementById("descriptions");
+//     var introButton = document.getElementById("intro_button");
 
-    loginDiv.style.display = "block";
-    descriptionsDiv.style.display = "none";
-    introButton.style.display = "none"
-}
+//     loginDiv.style.display = "block";
+//     descriptionsDiv.style.display = "none";
+//     introButton.style.display = "none"
+// }
 
-function home() {
-    var loginDiv = document.getElementById("login");
-    var descriptionsDiv = document.getElementById("descriptions");
-    var introButton = document.getElementById("intro_button");
+// function home() {
+//     var loginDiv = document.getElementById("login");
+//     var descriptionsDiv = document.getElementById("descriptions");
+//     var introButton = document.getElementById("intro_button");
 
-    loginDiv.style.display = "none";
-    descriptionsDiv.style.display = "block";
-    introButton.style.display = "inline"
-}
+//     loginDiv.style.display = "none";
+//     descriptionsDiv.style.display = "block";
+//     introButton.style.display = "inline"
+// }
