330ccc106e99f92e7e3dd06c14941c0262565fcf
hznkyh <hank021008@outlook.com>
2023-04-21 13:29:29 +0800

    reconstruting branch

---
+++ Login/scripts.js
@@ -0,0 +1,19 @@
+// function loginButton() {
+//     var loginDiv = document.getElementById("login");
+//     var descriptionsDiv = document.getElementById("descriptions");
+//     var introButton = document.getElementById("intro_button");
+
+//     loginDiv.style.display = "block";
+//     descriptionsDiv.style.display = "none";
+//     introButton.style.display = "none"
+// }
+
+// function home() {
+//     var loginDiv = document.getElementById("login");
+//     var descriptionsDiv = document.getElementById("descriptions");
+//     var introButton = document.getElementById("intro_button");
+
+//     loginDiv.style.display = "none";
+//     descriptionsDiv.style.display = "block";
+//     introButton.style.display = "inline"
+// }
