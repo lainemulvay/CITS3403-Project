dce1a1523c34fb95b50061b4e81b8840bfa1e7b9
hznkyh <hank021008@outlook.com>
2023-04-29 11:59:04 +0800

    migrated files for flask (login)

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("intro_view.html")
 
+@app.route("/login")
+def login():
+    return render_template("login_view.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
