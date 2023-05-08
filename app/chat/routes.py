5c7829e02ae9c036a6087331184f1a1eea623933
Murray Lewin <murraylewin91@gmail.com>
2023-05-08 18:12:37 +0800

    routes for chat history

--- app/chat/routes.py
+++ app/chat/routes.py
@@ -11,7 +11,25 @@ def chat():
     if 'email' not in session:
         flash('Please log in to view this page', 'danger')
         return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        
+        user_message = request.form['user_message']
+
+        # call chatbot API ******** still trying to integrate this ********
+        chatbot_response = requests.get('http://mychatbotapi.com', params={'user_message': user_message}).text
+
+        # create new chat and chat response instances
+        user_id = User.query.filter_by(email=session['email']).first().id
+        chat = Chat(user_id=user_id, text=user_message, created_at=datetime.utcnow())
+        chat_response = ChatResponse(chat_id=chat.id, text=chatbot_response, created_at=datetime.utcnow())
+
+        # add new instances to the database
+        db.session.add(chat)
+        db.session.add(chat_response)
+        db.session.commit()
     return render_template("chat_view.html", display = True)
+
 
 @chat_blueprint.route('/logout')
 def logout():
