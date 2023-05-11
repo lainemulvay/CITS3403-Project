c4d58fc66f5ea7d99f44f450a089c28484a95e53
hznkyh <hank021008@outlook.com>
2023-05-11 23:15:53 +0800

    adjusted returned value of add_chat function

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 from flask import url_for
-from app.model import User, Chat, ChatMessage
+from app.models import User, Chat, ChatMessage
 from app import db
 from flask_login import current_user, login_user, logout_user
 from sqlalchemy.orm.attributes import flag_modified
@@ -11,7 +11,7 @@ def add_chat(user_id):
     chat = Chat(user_id=user_id)
     db.session.add(chat)
     db.session.commit()
-    return chat
+    return chat.id
 
 def add_chat_message(chat_id, question, response):
     chat_message = ChatMessage(chat_id=chat_id, question=question, response=response)
