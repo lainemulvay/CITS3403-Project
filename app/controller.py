81920aafa2378ca6fba1ed8c3f36c283592fc123
hznkyh <hank021008@outlook.com>
2023-05-14 23:16:21 +0800

    fixed wrong attribute used

--- app/controller.py
+++ app/controller.py
@@ -35,3 +35,19 @@ def get_chat_ids(user_id):
     for chat in user.chat:
         id_list.append(chat.id)
     return id_list
+
+def get_chat_questions(chat_id):
+    chat = Chat.query.filter_by(id=chat_id).first()
+    questions = []
+    for question in chat.chat_questions:
+        question = [question.content, question.timestamp]
+        questions.append(question)
+    return questions
+
+def get_chat_responses(chat_id):
+    chat = Chat.query.filter_by(id=chat_id).first()
+    responses = []
+    for response in chat.chat_responses:
+        response = [response.content, response.timestamp]
+        responses.append(response)
+    return responses
