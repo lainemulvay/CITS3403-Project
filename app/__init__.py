aef080bd83ad15d84a053ee2bfc53e684b590150
hznkyh <hank021008@outlook.com>
2023-05-16 23:45:36 +0800

    app restructure

--- app/__init__.py
+++ app/__init__.py
@@ -5,31 +5,37 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.secret_key = secrets.token_urlsafe(32)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app.register import register_blueprint
-app.register_blueprint(register_blueprint)
+def initapp():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.secret_key = secrets.token_urlsafe(32)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    with app.app_context():
+        print("Creating database tables...")
+        db.create_all()
 
-from app.intro import intro_blueprint
-app.register_blueprint(intro_blueprint)
+    from app.register import register_blueprint
+    app.register_blueprint(register_blueprint)
 
-from app.chat import chat_blueprint
-app.register_blueprint(chat_blueprint)
+    from app.intro import intro_blueprint
+    app.register_blueprint(intro_blueprint)
 
-from app.login import login_blueprint
-app.register_blueprint(login_blueprint)
+    from app.chat import chat_blueprint
+    app.register_blueprint(chat_blueprint)
 
-from app.history import history_blueprint
-app.register_blueprint(history_blueprint)
+    from app.login import login_blueprint
+    app.register_blueprint(login_blueprint)
 
-from app.profile import profile_blueprint
-app.register_blueprint(profile_blueprint)
+    from app.history import history_blueprint
+    app.register_blueprint(history_blueprint)
+
+    from app.profile import profile_blueprint
+    app.register_blueprint(profile_blueprint)
+
+    return app
 
 from app import models
-
-with app.app_context():
-    db.create_all()
