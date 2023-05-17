1bfb60ae5fdcb2b3727d8bffa43f3e95490604af
hznkyh <hank021008@outlook.com>
2023-05-17 17:03:32 +0800

    add testing config

--- config.py
+++ config.py
@@ -6,3 +6,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SERVER_NAME = 'localhost.localdomain:5000'
