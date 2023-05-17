0090240c2d5be522e26b832afc0252f172241a67
hznkyh <hank021008@outlook.com>
2023-05-18 00:13:19 +0800

    add database fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,25 @@
 import os
 import pytest
-from app import initapp
+from app import initapp, db
+from app.models import User
 
 @pytest.fixture(scope='module')
 def test_client():
-    os.environ['CONFIGURATION'] = 'config.TestingConfig'
     flask_app = initapp()
+    flask_app.config.from_object('config.TestingConfig')
     
     with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            db.create_all()
         yield testing_client
+        with flask_app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+@pytest.fixture(scope='module')
+def test_user_table():
+    flask_app = initapp()
+    flask_app.config.from_object('config.TestingConfig')
+
+    with flask_app.app_context():
+        user = User(first_name='test', last_name='user',)
