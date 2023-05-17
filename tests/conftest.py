52459b0834a300fecb5deaf6035b14bb25ea62a5
hznkyh <hank021008@outlook.com>
2023-05-17 17:03:22 +0800

    add test_client

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import os
+import pytest
+from app import initapp
+
+@pytest.fixture(scope='module')
+def test_client():
+    os.environ['CONFIGURATION'] = 'config.TestingConfig'
+    flask_app = initapp()
+    testing_client = flask_app.test_client()
+    ctx = flask_app.app_context()
+    ctx.push()
+    yield testing_client
+    ctx.pop()
