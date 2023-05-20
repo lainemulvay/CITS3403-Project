8f358c275a49bc1cd86bd1b8814f78b66111fb14
hznkyh <hank021008@outlook.com>
2023-05-20 23:12:37 +0800

    commented

--- app/chat/training/docs/question_scrape_test.py
+++ app/chat/training/docs/question_scrape_test.py
@@ -1,29 +1,29 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import csv
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+# import csv
 
-faqList = []
+# faqList = []
 
-def getFAQs(identifier):
-    url = f'https://ipoint.uwa.edu.au/app/answers/detail/a_id/{identifier}'
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
+# def getFAQs(identifier):
+#     url = f'https://ipoint.uwa.edu.au/app/answers/detail/a_id/{identifier}'
+#     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
+#     r = requests.get(url, headers=headers)
+#     soup = BeautifulSoup(r.text, 'html.parser')
 
-    try:
-        faq = {
-            'question': soup.select('#rn_PageContent #ans_desc')[0].text.strip(),
-            'answer': soup.select('#rn_AnswerText')[0].text.strip()
-        }
-        faqList.append(faq)
-        print(f"Added Answer ID {identifier}")
-    except IndexError:
-        print(f"Skipping Answer ID {identifier} - Page not found")
+#     try:
+#         faq = {
+#             'question': soup.select('#rn_PageContent #ans_desc')[0].text.strip(),
+#             'answer': soup.select('#rn_AnswerText')[0].text.strip()
+#         }
+#         faqList.append(faq)
+#         print(f"Added Answer ID {identifier}")
+#     except IndexError:
+#         print(f"Skipping Answer ID {identifier} - Page not found")
 
-for x in range(1,50): #none past 1700
-    getFAQs(x)
+# for x in range(1,50): #none past 1700
+#     getFAQs(x)
 
-print(faqList)
-df = pd.DataFrame(faqList)
-print(df.head())
+# print(faqList)
+# df = pd.DataFrame(faqList)
+# print(df.head())
