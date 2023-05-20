5ded952966a7693bab6a7bbcc944337c4e6d66d5
LaineMulvay <22708032@student.uwa.edu.au>
2023-05-20 09:33:57 +0800

    updated find url

--- app/chat/training/train.py
+++ app/chat/training/train.py
@@ -5,12 +5,13 @@ import os
 
 os.environ["OPENAI_API_KEY"] = "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"
 
-faq_url = "C:/Users/61433/Desktop/UNI/CITS3403/Project/CITS3403-Project/app/chat/training/docs/faq.csv"
+#finds and reads faq.csv
+script_path = os.path.abspath(__file__)
+faq_path = os.path.join(os.path.dirname(script_path), 'docs', 'faq.csv')
+df = pd.read_csv(faq_path)
 
-df = pd.read_csv(faq_url)
-
+#inputs question to chatbot and runs on faq.csv
 pd_agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True)
-
 pd_agent.run("where are exams held?")
 
 
